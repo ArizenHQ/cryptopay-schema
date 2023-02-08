9a30dc2b78836d6d9f2d45ba068924d2769781d0
Alexandre Ouicher <aouicher@cobalt.fr>
2023-02-08 21:27:05 +0100

    expose order model

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Accounts = require('./src/accounts.model');
 const Users = require('./src/users.model');
 const Projects = require('./src/projects.model');
+const Orders = require('./src/orders.model');
+
 
 
 const exports = module.exports = {}
@@ -10,3 +12,5 @@ exports.Accounts = Accounts;
 exports.Users = Users;
 
 exports.Projects = Projects;
+
+exports.Orders = Orders;
