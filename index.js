14cdc6280b38270c15569da4971682d1af8efe48
Alexandre Ouicher <aouicher@cobalt.fr>
2023-02-08 21:54:44 +0100

    change portee variable

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Users = require('./src/users.model');
 const Projects = require('./src/projects.model');
 const Orders = require('./src/orders.model');
 
-const exports = module.exports = {}
+let exports = module.exports = {}
 
 exports.Accounts = Accounts;
 
