bd7c05c094341e3c385382aa11c372e1e5278011
Alexandre Ouicher <aouicher@cobalt.fr>
2025-03-25 15:14:17 +0100

    fix: enhance patchById method to include error handling and merge user data for updates

--- dist/src/users.model.js
+++ dist/src/users.model.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -109,10 +120,14 @@ var Users = /** @class */ (function () {
                     case 0: return [4 /*yield*/, this.User.get({ id: id }, { index: "gs4", follow: true })];
                     case 1:
                         user = _b.sent();
+                        if (!user)
+                            throw new Error("User with id ".concat(id, " not found"));
+                        // Set context avec l'account
                         this.table.setContext({ accountId: user.accountId });
-                        data.id = id;
-                        return [4 /*yield*/, this.User.update(data, { return: 'get' })];
-                    case 2: return [2 /*return*/, _b.sent()];
+                        return [4 /*yield*/, this.User.update(__assign({ id: user.id, accountId: user.accountId, email: user.email }, data), { return: 'get' })];
+                    case 2: 
+                    // Fusionne les données nécessaires à l'update
+                    return [2 /*return*/, _b.sent()];
                 }
             });
         }); };
